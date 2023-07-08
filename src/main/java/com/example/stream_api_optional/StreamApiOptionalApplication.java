64bbed3ee0928f0f5a04aa5436f852a33df53716
Yurii <shadowjura82@gmail.com>
2023-07-08 22:03:14 +0200

    Initial

---
+++ src/main/java/com/example/stream_api_optional/StreamApiOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.stream_api_optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiOptionalApplication.class, args);
+    }
+
+}
